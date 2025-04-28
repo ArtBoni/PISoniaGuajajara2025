ee03a3b8ed80fbe38767db4e0dcc42fc2ab313ae
JosueRFP <josue.petri.senac@gmail.com>
2025-04-28 14:10:42 -0300

    Adição do inimigo

---
+++ PI Guajajara/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            Vector2 direction= (target.position - transform.position).normalized;
+            transform.position =  Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
