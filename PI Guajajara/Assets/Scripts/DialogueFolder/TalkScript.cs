abd9e1220b9f8a0e2dd55fb60b55cac5a685bd0d
JosueRFP <josue.petri.senac@gmail.com>
2025-05-20 13:28:42 -0300

    Dialogo funciona

--- PI Guajajara/Assets/Scripts/DialogueFolder/TalkScript.cs
+++ PI Guajajara/Assets/Scripts/DialogueFolder/TalkScript.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -12,17 +14,54 @@ public class TalkScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        textComponent.text = string.Empty;
+        StartDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else 
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
     }
 
    public  void StartDialogue()
     {
         index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }    
     }
 }
+
