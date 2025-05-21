e9363c00ddba06c48ed456d2e6cc7ad5b656cf73
JosueRFP <josue.petri.senac@gmail.com>
2025-05-21 15:29:46 -0300

    Inicio da interação com o npc

--- PI Guajajara/Assets/Scripts/DialogueFolder/NpcInteraction.cs
+++ PI Guajajara/Assets/Scripts/DialogueFolder/NpcInteraction.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public enum Dialoguing
-{
-    TALKING, PLAYING
-}
+
 public class NpcInteraction : TalkScript
 {
+    [SerializeField] UnityEvent OnDialogueStart;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
