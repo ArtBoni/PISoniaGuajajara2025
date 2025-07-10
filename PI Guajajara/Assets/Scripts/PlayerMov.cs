20d256f632596bc190d92436726cd6ee19fbaaa8
JosueRFP <josue.petri.senac@gmail.com>
2025-07-10 13:52:16 -0300

    Melhorar a interação com o npc

--- PI Guajajara/Assets/Scripts/PlayerMov.cs
+++ PI Guajajara/Assets/Scripts/PlayerMov.cs
@@ -1,18 +1,14 @@
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))] 
-[RequireComponent(typeof(Rigidbody2D))] 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMov : MonoBehaviour
 {
     [SerializeField] float speed;
-    [SerializeField] SpriteRenderer spriteRenderer;
+    
 
-    float moveX;
-    float moveY;
     float horizontal, vertical;
-    bool facingX = true;
-    bool facingY = true;
-   
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,54 +22,18 @@ public class PlayerMov : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        moveX = Input.GetAxisRaw("Horizontal"); 
-        moveY = Input.GetAxisRaw("Vertical");
-        /*Vector2 moveDirection = new Vector2 (horizontal, vertical).normalized;
-        
-        if(moveDirection.magnitude >= 0)
-        {
-            return;
-        }
-        */
 
-        if (moveX > 0 && !facingX)
-            FlipX();
-
-        if (moveX < 0 && facingX)
-            FlipX();
-        
-           
-
-        if (moveY > 0 && !facingY)
-            FlipY();
-        
-        if(moveY < 0 && facingY)
-            FlipY();
-
-
-
-       
+        Vector2 direction = new Vector2(horizontal, vertical).normalized;
         
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2 (horizontal, vertical) * speed;
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
     }
 
-    void FlipX()
-    {
-        facingX = !facingX;
-        Vector3 theScaleX = transform.localScale;
-        theScaleX.x *= -1;
-        transform.localScale = theScaleX;
-    }
-
-    void FlipY()
-    {
-        facingY = !facingY;
-        Vector3 theScale = transform.localScale;
-        theScale.y *= -1;
-        transform.localScale = theScale;
-    }
+   public void Flip()
+   {
+        
+   }
 }
