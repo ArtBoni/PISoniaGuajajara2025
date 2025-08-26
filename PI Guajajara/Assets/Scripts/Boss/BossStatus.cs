5e3dcf8187d0d8955ba41e90405f0c365084521d
JosueRFP <josue.petri.senac@gmail.com>
2025-08-26 13:50:56 -0300

    Inicio da Boss Fight

---
+++ PI Guajajara/Assets/Scripts/Boss/BossStatus.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossStatus : MonoBehaviour
+{
+    [SerializeField] float health;
+    float timeBtwDamage = 1.5f;
+
+    [SerializeField] Slider healthBar;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
