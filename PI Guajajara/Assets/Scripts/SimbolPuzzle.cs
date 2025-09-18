f77a1bbb9e79e3cc5443800c1c6e73c34a8e0a80
JosueRFP <josue.petri.senac@gmail.com>
2025-09-18 16:37:39 -0300

    Update SimbolPuzzle.cs

--- PI Guajajara/Assets/Scripts/SimbolPuzzle.cs
+++ PI Guajajara/Assets/Scripts/SimbolPuzzle.cs
@@ -5,27 +5,27 @@ using UnityEngine.UI;
 public class SimbolPuzzle : MonoBehaviour
 {
     Image simbol;
+    [SerializeField] GameObject panel;
     private void Start()
     {
         simbol = GetComponent<Image>();
         StartCoroutine(WaitColorToChange());
-
-
+        panel.SetActive(false);
     }
 
     private void Update()
     {
+
     }
 
-  public IEnumerator WaitColorToChange()
-  {
-        float timeChenge = .5f;
-        while (true) {
+    public IEnumerator WaitColorToChange()
+    {
+        float timeChenge = 0f;
+        while (true) 
+        {
             simbol.color = Random.ColorHSV();
             yield return new WaitForSeconds(timeChenge);
         }
-
-        
-  }
+    }
 
 }
