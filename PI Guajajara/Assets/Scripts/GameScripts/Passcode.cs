311a1f8ba68c9fddd075005cfa52cac068a678c2
Bernardorocha0710 <167346087+Bernardorocha0710@users.noreply.github.com>
2025-09-23 13:11:03 -0300

    wow

---
+++ PI Guajajara/Assets/Scripts/GameScripts/Passcode.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Passcode : MonoBehaviour
+{
+    [SerializeField] string code;
+    string Nr = null;
+    int NrIndex = 0;
+    string alpha;
+    public TextMeshProUGUI UiText = null;
+
+
+    [SerializeField] GameObject doorObj, painelObj;
+
+    public void CodeFunction(string Numbers)
+    {
+        NrIndex++;
+        Nr = Nr + Numbers;
+        UiText.text = Nr;
+    }
+
+    public void Enter()
+    {
+        if (Nr == code)
+        {
+            doorObj.SetActive(false);
+        }
+    }
+    public void Delete()
+    {
+        NrIndex++;
+        Nr = null;
+        UiText.text = Nr;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+            Debug.Log("Player encostando no painel!");
+
+        painelObj.SetActive(true);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+            painelObj.SetActive(false);
+    }
+}
--- PI Guajajara/Assets/Scripts/PlayerScripts/WaterBorrifirer.cs
+++ PI Guajajara/Assets/Scripts/PlayerScripts/WaterBorrifirer.cs
@@ -15,22 +15,37 @@ public class WaterBorrifirer : MonoBehaviour
     [SerializeField] private AudioSource waterSound;
     [SerializeField] private AudioEvent OnWaterEvent;
 
+    private Camera mainCam;
+
+    private void Start()
+    {
+        mainCam = Camera.main; 
+    }
+
     private void Update()
     {
+        AimAtMouse();
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
     }
 
+    void AimAtMouse()
+    {
+        Vector3 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;   
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
     void Shoot()
     {
         GameObject bullet = Instantiate(waterPrefab, waterSpawnPoint.position, waterSpawnPoint.rotation);
         WaterProjetil proj = bullet.GetComponent<WaterProjetil>();
-
         if (proj != null)
             proj.SetDamage(damage);
-
+        waterSound?.Play();
         OnWaterEvent?.Invoke(waterSound);
     }
 }
