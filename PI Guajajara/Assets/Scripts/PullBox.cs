2ab9c76e18af4b0eaf85d724f53f2c0789466d9c
JosueRFP <josue.petri.senac@gmail.com>
2025-10-28 16:34:46 -0300

    Sokoban incompleto

--- PI Guajajara/Assets/Scripts/PullBox.cs
+++ PI Guajajara/Assets/Scripts/PullBox.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class PullBox : MonoBehaviour
 {
-    [SerializeField] Transform box;
+    [SerializeField] Transform sokobanObj;
+    [SerializeField] GameObject sokobanInstance;
     [SerializeField] int distance;
     bool isNearBox = false;
     private void Start()
@@ -13,6 +14,41 @@ public class PullBox : MonoBehaviour
 
     private void Update()
     {
-        
+
+
+
     }
-}
+
+
+
+
+
+    public void Sokoban()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            isNearBox = true;
+
+            if (isNearBox)
+            {
+                Vector3 direction = sokobanObj.position - transform.position;
+                direction.y = 0; // Keep the movement in the horizontal plane
+                direction.Normalize();
+                sokobanObj.position += direction * distance * Time.deltaTime;
+            }
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            sokobanInstance = collision.gameObject;
+            sokobanObj = sokobanInstance.transform;
+            isNearBox = true;
+        }
+    }
+}   
+
