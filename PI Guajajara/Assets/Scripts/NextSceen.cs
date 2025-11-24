de17aef646857045973645ffcb6e15a7ddfae28c
JosueRFP <josue.petri.senac@gmail.com>
2025-11-24 14:38:45 -0300

    Hud no 1° e 2° andar

---
+++ PI Guajajara/Assets/Scripts/NextSceen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceen : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Teleport()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Teleport();
+        }
+    }
+}
