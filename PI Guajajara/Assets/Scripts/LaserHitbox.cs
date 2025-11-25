5c735182599edc9eae882c22d78d1d1c69ce2082
Arthur Bonelli <artbonelli@gmail.com>
2025-11-25 08:53:07 -0300

    LaserStartMaking

---
+++ PI Guajajara/Assets/Scripts/LaserHitbox.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class LaserHitbox : MonoBehaviour
+{
+    public UnityEvent laserActive;
+    public GameObject alarms;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject)
+        {
+            laserActive.Invoke();
+            alarms.SetActive(true);
+        }
+    }
+}
